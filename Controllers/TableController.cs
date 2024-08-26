5f42f36c40f752f5593ba324466878268b6b54fe
Adrozs <adrozsahegyi@gmail.com>
2024-08-26 22:20:05 +0200

    Created table controller

--- Controllers/TableController.cs
+++ Controllers/TableController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RestaurantBooking.Models.DTOs;
+using RestaurantBooking.Models.DTOs.TableDTOs;
 using RestaurantBooking.Services.IServices;
 
 namespace RestaurantBooking.Controllers
