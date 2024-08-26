f34d0384b70363e21d3b3d52a6239ceb2c8217a6
Adrozs <adrozsahegyi@gmail.com>
2024-08-26 22:20:43 +0200

    Created customer repo interface and class

---
+++ Data/Repos/CustomerRepo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos
+{
+    public class CustomerRepo : ICustomerRepo
+    {
+        private readonly BookingDbContext _context;
+
+        public CustomerRepo(BookingDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CreateCustomerAsync(Customer customer)
+        {
+            await _context.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCustomerAsync(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(int customerId)
+        {
+            return await _context.Customers.SingleOrDefaultAsync(c => c.Id == customerId);
+        }
+
+        public async Task UpdateCustomerAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Data/Repos/IRepos/ICustomerRepo.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos.IRepos
+{
+    public interface ICustomerRepo
+    {
+        public Task CreateCustomerAsync(Customer customer);
+        public Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        public Task<Customer> GetCustomerByIdAsync(int customerId);
+        public Task UpdateCustomerAsync(Customer customer);
+        public Task DeleteCustomerAsync(Customer customer);
+    }
+}
