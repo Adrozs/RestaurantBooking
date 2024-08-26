77678ed7ffd946e0d0e22a31f892c7bdff8838e6
Adrozs <adrozsahegyi@gmail.com>
2024-08-26 22:23:24 +0200

    Removed unused dto

--- Models/DTOs/TableNoIdDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantBooking.Models.DTOs
-{
-    public class TableNoIdDTO
-    {
-        public int TableNumber { get; set; }
-        public int Seats { get; set; }
-        public bool IsReserved { get; set; }
-    }
-}
