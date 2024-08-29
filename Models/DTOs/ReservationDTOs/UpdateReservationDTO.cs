83809f02668dcd67e2c1a5dce895e6eddb11c192
Adrozs <adrozsahegyi@gmail.com>
2024-08-29 12:15:24 +0200

    Added dto specific for updating reservations (without customer name as that is retrieved from db not entered manually)

---
+++ Models/DTOs/ReservationDTOs/UpdateReservationDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantBooking.Models.DTOs.ReservationDTOs
+{
+    public class UpdateReservationDTO
+    {
+        public int Id { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public int ReservationDurationMinutes { get; set; }
+        public int Guests { get; set; }
+        public decimal TotalBill { get; set; }
+
+        public int TableId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
